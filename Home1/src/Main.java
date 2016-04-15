7a3cd128767cf68a9d9ef14b7a7e047a5a84b50a
sayberex <sayberex007@ya.ru>
2016-04-15 15:01:38 +0300

    commit 2 from work

--- Home1/src/Main.java
+++ Home1/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Program Entry:");
 		System.out.println("Downloaded from home:");
+		
+		System.out.println("added code from work");
 	}
 
 }
